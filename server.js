41d99ac0204e9f74ad4a8f993081d8e21ffb6012
Zilong Huang <zilonghuang6@gmail.com>
2020-10-16 22:44:02 -0400

    App.yaml Addition

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 const INDEX = '/index.html';
 
 const server = express()
